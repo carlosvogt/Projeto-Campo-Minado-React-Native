b1d965163beb3c76b03e5c2b315a83f58977c419
carlosvogt <carlosvogt15@gmail.com>
2021-12-21 21:28:12 -0300

    Configuração ESLINT

---
+++ README.md
@@ -0,0 +1,17 @@
+# Campo Minado em React-Native
+
+## Sobre o projeto
+
+Desenvolvido durante a realização do curso de React-native Desenvolva Apps Nativos para Android e iOS da plataforma Udemy.
+
+<br>
+<br>
+<li> Abaixo será adicionada a imagem final do projeto.
+<br>
+<br>
+<!--
+    <div>
+     <img alt="Screenshot" title="#Screenshot" src="https://github.com/carlosvogt/Projeto-Calculadora-React-Native/blob/main/screenshot.PNG "
+    width="300"/>
+    </div> 
+-->
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+const ModuleResolverPlugin = [
+  'module-resolver',
+    {
+    root: ['./src'],
+    extensions: ['.js', '.jsx', '.ios.js', '.android.js', '.json'],
+    alias: {},
+    },
+  ];
+
+  module.exports = {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [ModuleResolverPlugin],
+    env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+    },
+  };
